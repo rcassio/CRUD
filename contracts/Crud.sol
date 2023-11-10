3141c75b55a59c05a30d31956d4a40f5874e644c
Cassio <cassio2508@gmail.com>
2023-11-10 09:48:36 -0300

    Crud Smart Contract and Unit Test

--- contracts/Crud.sol
+++ contracts/Crud.sol
@@ -2,7 +2,44 @@
 pragma solidity ^0.8.0;
 
 contract Crud {
+
+    struct User {
+        uint id;
+        string name;
+    }
+
+    User[] public users;
+    uint public nextId = 1;
+
+    function create(string memory name) public {
+        users.push(User(nextId, name));
+        nextId++;
+    }
     
+    function read(uint id) view public returns(uint, string memory) {
+        uint index = find(id);
+        return(users[index].id, users[index].name);
+    }
+
+    function update(uint id, string memory name) public {
+        uint index = find(id);
+        users[index].name = name;
+    }
+
+    function destroy(uint id) public {
+        uint index = find(id);
+        delete users[index];
+    }
+
+    function find(uint id) view public returns(uint index) {
+        for (uint i; i < users.length; i++) {
+            if(users[i].id == id) {
+                return i;
+            }
+        }
+        revert('User not found');
+    }
+
     function test() pure public returns(string memory) {
         return "test";
     }
--- test/crud-test.js
+++ test/crud-test.js
@@ -1,4 +1,4 @@
-const Crud = artifacts.require("Crud");
+const Crud = artifacts.require('Crud');
 
 contract('Crud', () => {
   let instance  = null;
@@ -6,8 +6,53 @@ contract('Crud', () => {
     instance = await Crud.deployed();
   });
 
-  it("should do test", async () => {
+  it('should do test', async () => {
     const result = await instance.test();
-    assert(result == "test");
+    assert(result == 'test');
+  });
+
+  it('should create a new user', async () => {
+    await instance.create('Frank');
+    const user = await instance.read(1);
+    assert(user[0].toNumber() === 1);
+    assert(user[1] === 'Frank');
+  });
+
+  it('Should update a user', async () => {
+    await instance.update(1, 'Isabela');
+    const user = await instance.read(1);
+    assert(user[0].toNumber() === 1);
+    assert(user[1] === 'Isabela');
+  });
+
+  it('Should not update a non-existing user', async () => {
+    try {
+      await instance.update(2, 'Frank');
+    } catch (e) {
+      assert(e.message.includes('User not found'));
+      return;
+    }
+    assert(false);
+  });
+
+  it('Should destroy a user', async () => {
+    await instance.destroy(1);
+    try {
+      await instance.read(1);
+    } catch (e) {
+      assert(e.message.includes('User not found'));
+      return;
+    }
+    assert(false);
+  });
+
+  it('Should not destroy a non-existing user', async () => {
+    try {
+      await instance.destroy(10);
+    } catch (e) {
+      assert(e.message.includes('User not found'));
+      return;
+    }
+    assert(false);
   });
 });
